dc433862ff9c548e06e14fa6eccf71201137775e
Nick Khaetsky <rzzsapb@gmail.com>
2020-06-03 13:31:20 +0300

    1.0.6: Fixing Link plugin is not being rendered

--- routes.php
+++ routes.php
@@ -4,8 +4,8 @@ Route::group(['prefix' => 'editorjs'], function (){
 
     Route::group(['prefix' => 'plugins'], function (){
 
-        Route::any('linktool', function ($code){
-            return (new ReaZzon\Editor\Classes\Plugins\LinkTool\Plugin)->createResponse($code);
+        Route::any('linktool', function (){
+            return (new ReaZzon\Editor\Classes\Plugins\LinkTool\Plugin)->createResponse(\Input::all());
         });
 
     });
