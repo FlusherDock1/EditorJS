dc48fdc858e95101b07384e9d4d067f5c3c56ce2
Nick Khaetsky <rzzsapb@gmail.com>
2022-08-04 21:00:38 +0300

    1.9.4: Fix compatibility issues with October CMS v3

--- classes/plugins/attaches/resources/AttachResource.php
+++ classes/plugins/attaches/resources/AttachResource.php
@@ -1,14 +1,14 @@
 <?php namespace ReaZzon\Editor\Classes\Plugins\Attaches\Resources;
 
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 use System\Models\File;
 
 /**
  * Class AttachResource
  * @package ReaZzon\Editor\Classes\Plugins\Attaches\Resources
  */
-class AttachResource extends Resource
+class AttachResource extends JsonResource
 {
     /**
      * @var string
